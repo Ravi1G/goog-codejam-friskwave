454f2c8e9d5d024ca7978c3484df8ae87739df17
arora.parth@gmail.com <arora.parth@gmail.com@ed205276-0b29-1ddf-5d40-206219bc8509>
2013-04-10 06:51:16 +0000

    - first code check-in to repository

---
+++ trunk/src/com/friskwave/utils/NumberOperations.java
@@ -0,0 +1,38 @@
+package com.friskwave.utils;
+
+import java.util.HashMap;
+
+/**
+ * @author Parth Arora
+ * @email arora.parth@gmail.com
+ * @desc This class contains generic implementations for certain mathematical
+ * operations that might come of use in multiple places.
+ */
+public class NumberOperations{
+	
+	public long HCF(long p, long q){
+		
+		if (q == 0) {
+			return p;
+		}
+		
+		return HCF(q, p % q);
+	}
+	
+	public HashMap<Integer,Long> getPrimeFactors(long number){
+		
+		long n = number;
+		
+		HashMap<Integer,Long> factors = new HashMap<Integer,Long>();
+		
+		int  k = 0;
+		for (long i = 2; i <= n; i++) {
+			while (n % i == 0) {
+				factors.put(k++,i);
+				n /= i;
+			}
+		}
+		
+		return factors;
+	}
+}
